c8a1ed987ba43eade2616722255cb2aaa6f508cb
Eduardo Derisso <dudu.derisso@gmail.com>
2024-08-26 15:07:08 -0300

    feature[MainActivity]: created feature to change input fiels visible depending on the 'formacao' value

--- app/src/main/java/com/example/havagas/MainActivity.kt
+++ app/src/main/java/com/example/havagas/MainActivity.kt
@@ -39,57 +39,108 @@ class MainActivity : AppCompatActivity() {
                 nomeOrientadorEt.setText("")
                 vagasInteresseEt.setText("")
             }
-
-            amb.formacaoSp.onItemSelectedListener = object: OnItemSelectedListener{
-                override fun onItemSelected(parent: AdapterView<*>?,
-                                            view: View?,
-                                            position: Int,
-                                            id: Long) {
-                    formacao = when(position) {
-                        0 -> "Indefinido"
-                        else -> amb.formacaoSp.selectedItem.toString()
-                    }
+        }
+        amb.formacaoSp.onItemSelectedListener = object: OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?,
+                                        view: View?,
+                                        position: Int,
+                                        id: Long) {
+                formacao = when(position) {
+                    0 -> "Indefinido"
+                    else -> amb.formacaoSp.selectedItem.toString()
                 }
+                System.out.println(formacao)
+                with (amb) {
+                    if (formacao == "fundamental" || formacao == "ensino medio") {
+                        anoFormaturaTv.visibility = View.VISIBLE
+                        anoFormaturaEt.visibility = View.VISIBLE
+                    } else {
+                        anoFormaturaTv.visibility = View.GONE
+                        anoFormaturaEt.visibility = View.GONE
+                        anoFormaturaEt.setText("")
+                    }
 
-                override fun onNothingSelected(p0: AdapterView<*>?) {
-                    //NSA
+
+                    if (formacao == "graduacao" || formacao == "especializacao" || formacao == "mestrado" || formacao == "doutorado") {
+                        anoConclusaoTv.visibility = View.VISIBLE
+                        anoConclusaoEt.visibility = View.VISIBLE
+
+                        instituicaoTv.visibility = View.VISIBLE
+                        instituicaoEt.visibility = View.VISIBLE
+                    } else{
+                        anoConclusaoTv.visibility = View.GONE
+                        anoConclusaoEt.visibility = View.GONE
+                        anoConclusaoEt.setText("")
+
+                        instituicaoTv.visibility = View.GONE
+                        instituicaoEt.visibility = View.GONE
+                        instituicaoEt.setText("")
+
+                    }
+
+
+                    if (formacao == "mestrado" || formacao == "doutorado") {
+                        nomeOrientadorTv.visibility = View.VISIBLE
+                        nomeOrientadorEt.visibility = View.VISIBLE
+
+                        tituloMonografiaTv.visibility = View.VISIBLE
+                        tituloMonografiaEt.visibility = View.VISIBLE
+                    } else {
+                        anoConclusaoEt.setText("")
+                        instituicaoEt.setText("")
+
+                        nomeOrientadorTv.visibility = View.GONE
+                        nomeOrientadorEt.visibility = View.GONE
+                        nomeOrientadorEt.setText("")
+
+
+                        tituloMonografiaTv.visibility = View.GONE
+                        tituloMonografiaEt.visibility = View.GONE
+                        tituloMonografiaEt.setText("")
+                    }
                 }
             }
 
-            amb.salvaBt.setOnClickListener {
-                with(amb) {
-                    val nomeCompleto =  nomeCompletoEt.text.toString()
-                    val email = emailEt.text.toString()
-                    val listaEmail = when (listaEmailCb.isChecked) {
-                        true -> "Tem interesse em recever emails com oportunidades"
-                        false -> "Não tem interesse em recever emails com oportunidades"
-                    }
-                    val telefone = telefoneEt.text.toString()
-                    val telefoneCelular = when (telefoneCelularCb.isChecked) {
-                        false -> "Indefinido"
-                        true -> telefoneCelularEt.text.toString()
-                    }
-                    val selectedSexoId = sexoRg.checkedRadioButtonId
-                    val sexo = when (selectedSexoId) {
-                        R.id.masculinoRb -> "Masculino"
-                        R.id.femininoRb -> "Feminino"
-                        else -> "Indefinido"
-                    }
-                    // Todo pegar data de nascimento
-                    //TODO restante da coleta de dados
-                    /*
-                    formacaoSp.setSelection(0)
-                    anoConclusaoEt.setText("")
-                    instituicaoEt.setText("")
-                    tituloMonografiaEt.setText("")
-                    nomeOrientadorEt.setText("")
-                    vagasInteresseEt.setText("")
-                    */
-                     */
-                }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                //NSA
             }
+        }
 
 
+
+        amb.salvaBt.setOnClickListener {
+            with(amb) {
+                val nomeCompleto =  nomeCompletoEt.text.toString()
+                val email = emailEt.text.toString()
+                val listaEmail = when (listaEmailCb.isChecked) {
+                    true -> "Tem interesse em recever emails com oportunidades"
+                    false -> "Não tem interesse em recever emails com oportunidades"
+                }
+                val telefone = telefoneEt.text.toString()
+                val telefoneCelular = when (telefoneCelularCb.isChecked) {
+                    false -> "Indefinido"
+                    true -> telefoneCelularEt.text.toString()
+                }
+                val selectedSexoId = sexoRg.checkedRadioButtonId
+                val sexo = when (selectedSexoId) {
+                    R.id.masculinoRb -> "Masculino"
+                    R.id.femininoRb -> "Feminino"
+                    else -> "Indefinido"
+                }
+                // Todo pegar data de nascimento
+
+                val formacao = formacaoSp.selectedItem.toString()
+                var anoFormacao = anoConclusaoEt.text.toString()
+                var  anoConlusao = anoConclusaoEt.text.toString()
+                var instituicao = instituicaoEt.text.toString()
+                var tituloMonografia = tituloMonografiaEt.text.toString()
+                var nomeOrientador = nomeOrientadorEt.text.toString()
+
+                val vagasInteresse = vagasInteresseEt.text.toString()
+
+                // TODO format string and toast
+
+            }
         }
     }
 }
